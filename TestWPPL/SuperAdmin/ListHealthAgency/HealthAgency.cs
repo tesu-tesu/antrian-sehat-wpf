a7833a8c53e86221ab96c655d95e505be12b496e
satriyoaji <satriyoaji27@gmail.com>
2020-11-29 06:10:10 +0700

    List Health Agency done and change some property UI on datagrid

---
+++ TestWPPL/SuperAdmin/ListHealthAgency/HealthAgency.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWPPL.SuperAdmin.ListHealthAgency
+{
+    public class HealthAgency
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public object image { get; set; }
+        public string call_center { get; set; }
+        public string email { get; set; }
+        //public DateTime created_at { get; set; }
+        //public DateTime updated_at { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int current_page { get; set; }
+        public List<HealthAgency> data { get; set; }
+        public string first_page_url { get; set; }
+        public int from { get; set; }
+        public int last_page { get; set; }
+        public string last_page_url { get; set; }
+        public object next_page_url { get; set; }
+        public string path { get; set; }
+        public int per_page { get; set; }
+        public string prev_page_url { get; set; }
+        public int to { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Root
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public Pagination data { get; set; }
+    }
+}
---
+++ TestWPPL/SuperAdmin/ListHealthAgency/ListHealthAgencyController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+
+namespace TestWPPL.SuperAdmin.ListHealthAgency
+{
+    class ListHealthAgencyController : MyController
+    {
+        public ListHealthAgencyController(IMyView _myView) : base(_myView)
+        {
+
+        }
+
+        public async void fetchDataHealthAgency()
+        {
+            ApiClient client = ApiAntrianSehat.getInstance().GetApiClient();
+            var request = new ApiRequestBuilder();
+
+            var req = request
+                .buildHttpRequest()
+                .setEndpoint("admin/health-agency")
+                .setRequestMethod(HttpMethod.Get);
+            client.setOnSuccessRequest(setViewSuccessFetch);
+            client.setOnFailedRequest(setViewErrorFetch);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+
+        }
+
+        private void setViewErrorFetch(HttpResponseBundle _response)
+        {
+            Console.WriteLine("err: " + _response.getHttpResponseMessage().Content.ReadAsStringAsync().Result);
+        }
+
+        private void setViewSuccessFetch(HttpResponseBundle _response)
+        {
+            Console.WriteLine("sukses: " + _response.getJObject());
+            Pagination healthAgencyPagination = _response.getParsedObject<Root>().data;
+
+            getView().callMethod("setListView", healthAgencyPagination);
+        }
+
+    }
+}
