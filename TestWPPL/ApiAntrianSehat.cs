f9b1cd7da90f1519f74ff8b8589a67f5a818c6d4
satriyoaji <satriyoaji27@gmail.com>
2020-11-28 15:39:40 +0700

    add ApiCLient singleton

---
+++ TestWPPL/ApiAntrianSehat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+
+namespace TestWPPL
+{
+    public class ApiAntrianSehat
+    {
+        private static ApiAntrianSehat instance;
+        private ApiClient apiClient;
+
+        private ApiAntrianSehat()
+        {
+            apiClient = new ApiClient("http://127.0.0.1:8000/api/");
+        }
+
+
+        public static ApiAntrianSehat getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new ApiAntrianSehat();
+            }
+            return instance;
+        }
+
+        public ApiClient GetApiClient()
+        {
+            return this.apiClient;
+        }
+
+    }
+
+}
