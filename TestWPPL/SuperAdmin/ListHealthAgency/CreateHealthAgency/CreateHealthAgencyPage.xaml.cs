8b845a8212a6e4eba40595798a572a44bc17ae3f
satriyoaji <satriyoaji27@gmail.com>
2020-12-02 09:41:25 +0700

    create health agency view and process logic on progress

--- TestWPPL/SuperAdmin/ListHealthAgency/CreateHealthAgency/CreateHealthAgencyPage.xaml.cs
+++ TestWPPL/SuperAdmin/ListHealthAgency/CreateHealthAgency/CreateHealthAgencyPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Velacro.UIElements.Basic;
+using Velacro.UIElements.Button;
+using Velacro.UIElements.ComboBox;
+using Velacro.UIElements.TextBlock;
+using Velacro.UIElements.TextBox;
 
 namespace TestWPPL.SuperAdmin.ListHealthAgency.CreateHealthAgency
 {
@@ -21,9 +26,49 @@ namespace TestWPPL.SuperAdmin.ListHealthAgency.CreateHealthAgency
     /// </summary>
     public partial class CreateHealthAgencyPage : MyPage
     {
+        private BuilderButton buttonBuilder;
+        private BuilderTextBox txtBoxBuilder;
+        private IMyButton addButton;
+        private IMyTextBox nameTxtBox, emailTxtBox, addressTxtBox, callCenterTxtBox;
+
         public CreateHealthAgencyPage()
         {
             InitializeComponent();
+            this.KeepAlive = true;
+            setController(new CreateHealthAgencyController(this));
+            initUIBuilders();
+            initUIElements();
+        }
+
+        private void initUIBuilders()
+        {
+            buttonBuilder = new BuilderButton();
+            txtBoxBuilder = new BuilderTextBox();
+        }
+
+        private void initUIElements()
+        {
+            addButton = buttonBuilder.activate(this, "addHA_btn");
+            nameTxtBox = txtBoxBuilder.activate(this, "name_txt");
+            emailTxtBox = txtBoxBuilder.activate(this, "email_txt");
+            addressTxtBox = txtBoxBuilder.activate(this, "address_txt");
+            callCenterTxtBox = txtBoxBuilder.activate(this, "call_center_txt");
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        public void btAddHA(object sender, RoutedEventArgs e)
+        {
+            getController().callMethod("storeHealthAgencyData",
+                name_txt.Text, email_txt.Text, address_txt.Text, call_center_txt.Text);
+        }
+        public void cek()
+        {
+            Console.WriteLine("ada");
         }
     }
 }
