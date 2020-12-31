daecd3f8d55d5c2cd99fbf6fa6db65f408b0a479
Re <refanopradana@gmail.com>
2020-12-31 23:28:14 +0700

    Controller PolyMaster CRUD

---
+++ TestWPPL/SuperAdmin/ListPolyMaster/ListPolyMasterController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using TestWPPL.Admin.CreatePolyclinicSchedule;
+using TestWPPL.SuperAdmin.ListHealthAgency;
+using Velacro.Basic;
+using Velacro.Api;
+
+namespace TestWPPL.SuperAdmin.ListPolyMaster
+{
+    public class ListPolyMasterController : MyController
+    {
+        private ApiClient client;
+        
+        public ListPolyMasterController(IMyView _myView) : base(_myView)
+        {
+            
+        }
+        
+        public async void fetchPolyMasterData(int idPolyMaster)
+        {
+            client = ApiAntrianSehat.getInstance().GetApiClient();
+            var request = new ApiRequestBuilder();
+
+            var req = request.buildHttpRequest()
+                .setEndpoint("admin/poly-master/")
+                .setRequestMethod(HttpMethod.Get);
+            client.setOnSuccessRequest(setViewSuccessFetch);
+            client.setOnFailedRequest(setViewFailedFetch);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+        }
+
+        private void setViewFailedFetch(HttpResponseBundle obj)
+        {
+            Console.WriteLine("err: " + obj.getHttpResponseMessage().Content.ReadAsStringAsync().Result);
+        }
+
+        private void setViewSuccessFetch(HttpResponseBundle _response)
+        {
+            Pagination polyMasterPagination = _response.getParsedObject<RootPolyMasterPagination>().data;
+            getView().callMethod("setListView", polyMasterPagination);
+        }
+        
+        public async void deleteProcess(PolyMaster polyMaster)
+        {
+            
+            ApiClient client = ApiAntrianSehat.getInstance().GetApiClient();
+            var request = new ApiRequestBuilder();
+
+            var req = request.buildHttpRequest()
+                .setEndpoint("admin/poly-master/" + polyMaster.id)
+                .setRequestMethod(HttpMethod.Delete);
+            client.setOnSuccessRequest(setViewSuccessDelete);
+            client.setOnFailedRequest(setViewErrorDelete);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+        }
+
+        private void setViewErrorDelete(HttpResponseBundle obj)
+        {
+            Console.WriteLine("err: " + obj.getHttpResponseMessage().Content.ReadAsStringAsync().Result);
+        }
+
+        private void setViewSuccessDelete(HttpResponseBundle obj)
+        {
+            getView().callMethod("setSuccessDelete", "Sukses menghapus Poly Master");
+        }
+    }
+}
--- TestWPPL/SuperAdmin/ListPolyMaster/PolyMaster.cs
+++ TestWPPL/SuperAdmin/ListPolyMaster/PolyMaster.cs
@@ -1,7 +1,52 @@
-﻿namespace TestWPPL.SuperAdmin.ListPolyMaster
+﻿using System;
+using System.Collections.Generic;
+using TestWPPL.Admin.CreatePolyclinicSchedule;
+using TestWPPL.SuperAdmin.ListHealthAgency;
+
+namespace TestWPPL.SuperAdmin.ListPolyMaster
 {
-    public class PolyMaster
+    /*public class PolyMaster
     {
-        
+        public int id { get; set; }
+        public String name { get; set; }
+    }*/
+
+
+    public class Pagination
+    {
+        public int current_page { get; set; }
+        public List<PolyMaster> data { get; set; }
+        public string first_page_url { get; set; }
+        public int from { get; set; }
+        public int last_page { get; set; }
+        public string last_page_url { get; set; }
+        public object next_page_url { get; set; }
+        public string path { get; set; }
+        public int per_page { get; set; }
+        public string prev_page_url { get; set; }
+        public int to { get; set; }
+        public int total { get; set; }
     }
+
+    public class RootPolyMasterPagination
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public Pagination data { get; set; }
+    }
+    
+    public class RootSinglePolyMaster
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public PolyMaster data { get; set; }
+    }
+    
+    public class RootPolyMasterList
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public List<PolyMaster> data { get; set; }
+    }
+
 }
