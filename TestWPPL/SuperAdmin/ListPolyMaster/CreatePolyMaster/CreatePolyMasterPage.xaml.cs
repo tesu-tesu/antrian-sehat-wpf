1a4127ae98d11fa9306220c39ba442841c5b37ef
Re <refanopradana@gmail.com>
2021-01-03 18:14:48 +0700

    Poly Master Superadmin

--- TestWPPL/SuperAdmin/ListPolyMaster/CreatePolyMaster/CreatePolyMasterPage.xaml.cs
+++ TestWPPL/SuperAdmin/ListPolyMaster/CreatePolyMaster/CreatePolyMasterPage.xaml.cs
@@ -1,12 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Velacro.UIElements.Basic;
 
 namespace TestWPPL.SuperAdmin.ListPolyMaster.CreatePolyMaster
 {
-    public partial class CreatePolyMaster : UserControl
+    public partial class CreatePolyMasterPage : MyPage
     {
-        public CreatePolyMaster()
+        public CreatePolyMasterPage()
         {
             InitializeComponent();
+        }
+
+        private void btAddPolyMaster(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
