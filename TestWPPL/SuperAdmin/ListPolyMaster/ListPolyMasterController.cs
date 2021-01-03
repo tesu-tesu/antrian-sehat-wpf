f204d2ef011b66224145159b751f9b275294b83b
Re <refanopradana@gmail.com>
2021-01-03 17:32:22 +0700

    List Fetch

--- TestWPPL/SuperAdmin/ListPolyMaster/ListPolyMasterController.cs
+++ TestWPPL/SuperAdmin/ListPolyMaster/ListPolyMasterController.cs
@@ -18,7 +18,7 @@ namespace TestWPPL.SuperAdmin.ListPolyMaster
             
         }
         
-        public async void fetchPolyMasterData(int idPolyMaster)
+        public async void fetchDataPolyMaster(int idPolyMaster)
         {
             client = ApiAntrianSehat.getInstance().GetApiClient();
             var request = new ApiRequestBuilder();
