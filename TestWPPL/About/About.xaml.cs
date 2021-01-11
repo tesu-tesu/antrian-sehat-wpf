264e74ef0c81eda3dab40b5a1170ee0b4a9f1e0f
Re <refanopradana@gmail.com>
2021-01-11 19:06:13 +0700

    Basic About

---
+++ TestWPPL/About/About.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Velacro.UIElements.Basic;
+
+namespace TestWPPL.About
+{
+    public partial class About : MyPage
+    {
+        public About()
+        {
+            InitializeComponent();
+            setController(new AboutController(this));
+        }
+    }
+}
---
+++ TestWPPL/About/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Velacro.Basic;
+
+namespace TestWPPL.About
+{
+    public class AboutController : MyController
+    {
+        public AboutController(IMyView _myView) : base(_myView)
+        {
+            
+        }
+    }
+}
